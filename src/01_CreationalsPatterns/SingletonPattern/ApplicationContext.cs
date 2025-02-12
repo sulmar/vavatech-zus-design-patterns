5942e48a90d84e3f049cb3fea8acabb60669a673
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-02-12 09:46:24 +0100

    Dodanie przykładu antywzorca

---
+++ src/01_CreationalsPatterns/SingletonPattern/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SingletonPattern;
+
+public class ApplicationContext : Singleton<ApplicationContext>
+{
+    public string Logged { get; set; }
+    public DateTime LoggedOn { get; set; }
+    
+    // Zła praktyka! Antywzorzec
+    public int SelectedCustomerId { get; set; }
+    public int SelectedOrderId { get; set; }
+}
