ed372e8772ded7d884203306bf8c83a80bd25ba7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-02-12 11:16:38 +0100

    Dodanie wewnętrznej fabryki

--- src/01_CreationalsPatterns/FactoryPattern/Models/Coordinate.cs
+++ src/01_CreationalsPatterns/FactoryPattern/Models/Coordinate.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace FactoryPattern.Models
 {
-	public partial class Coordinate
+    public class Test
+    {
+        public void DoWork()
+        {            
+            Coordinate coordinate = Coordinate.Factory.NewFromWkt("POINT (1, 2)");
+        }
+    }
+
+
+    public partial class Coordinate
 	{
 		// Długość geograficzna	
 		public double Longitude { get; }
@@ -22,50 +27,52 @@ namespace FactoryPattern.Models
 			this.Latitude = lat;
 		}
 
-		// Problem - logika związana z wytwarzaniem obiektów jest pomieszana z modelem danych
+        public class Factory
+        {
+            public static Coordinate NewFromWkt(string wkt)
+            {
+                const string pattern = @"POINT \((\d*)\s(\d*)\)";
 
-		public static Coordinate NewFromWkt(string wkt)
-		{
-			const string pattern = @"POINT \((\d*)\s(\d*)\)";
+                Regex regex = new Regex(pattern);
 
-			Regex regex = new Regex(pattern);
+                Match match = regex.Match(wkt);
 
-			Match match = regex.Match(wkt);
+                if (match.Success)
+                {
+                    double lng = double.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+                    double lat = double.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
 
-			if (match.Success)
-			{
-				double lng = double.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
-				double lat = double.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+                    return new Coordinate(lng, lat);
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
 
-				return new Coordinate(lng, lat);
-			}
-			else
-			{
-				throw new FormatException();
-			}
-		}
+            public static Coordinate NewFromGeoJson(string geojson)
+            {
+                const string pattern = @"\[(\d*), (\d*)\]";
 
-		public static Coordinate NewFromGeoJson(string geojson)
-		{
-			const string pattern = @"\[(\d*), (\d*)\]";
-
-			Regex regex = new Regex(pattern);
+                Regex regex = new Regex(pattern);
 
 
-			Match match = regex.Match(geojson);
+                Match match = regex.Match(geojson);
 
-			if (match.Success)
-			{
+                if (match.Success)
+                {
 
-				double lng = double.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
-				double lat = double.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
+                    double lng = double.Parse(match.Groups[1].Value, CultureInfo.InvariantCulture);
+                    double lat = double.Parse(match.Groups[2].Value, CultureInfo.InvariantCulture);
 
-				return new Coordinate(lng, lat);
-			}
-			else
-			{
-				throw new FormatException();
-			}
-		}
-	}
+                    return new Coordinate(lng, lat);
+                }
+                else
+                {
+                    throw new FormatException();
+                }
+            }
+        }
+
+    }
 }
