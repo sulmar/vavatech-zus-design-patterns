14fdf48036f2143e948baca168f8f55aac7de6fe
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-02-12 14:41:44 +0100

    Dodanie dto

---
+++ src/02_StructuralsPatterns/AdapterPattern/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Adapter
+    public class CustomerMapper
+    {
+        public Customer Map(CustomerDto dto)
+        {
+            return new Customer { Id = dto.Id, Name = $"{dto.FirstName} {dto.LastName}" };
+        }
+
+    }
+
+    internal class CustomersController
+    {
+
+        public void Post(CustomerDto dto)
+        {
+            CustomerMapper mapper = new CustomerMapper();
+            Customer customer = mapper.Map(dto);
+
+        }
+    }
+}
