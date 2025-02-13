902a48609043313827029c0af3cc0312869e3524
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-02-13 07:45:53 +0100

    Dodanie przykładu wypożyczenia

---
+++ src/TDD/TestApp/Rent.cs
@@ -0,0 +1,28 @@
+﻿namespace TestApp
+{
+    public class Rent
+    {
+        public User Rentee { get; set; }
+
+        public bool CanReturn(User user)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+
+            if (user.IsAdmin)
+                return true;
+
+            if (Rentee == user)
+                return true;
+
+            return false;
+        }
+
+    }
+
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
