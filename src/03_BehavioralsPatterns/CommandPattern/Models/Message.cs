dc1ee8c88a21adc6e7ea58b2bf1d4d25dc8fc2ef
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-02-14 09:43:48 +0100

    Dodanie kompozytu komend

--- src/03_BehavioralsPatterns/CommandPattern/Models/Message.cs
+++ src/03_BehavioralsPatterns/CommandPattern/Models/Message.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CommandPattern
 {
@@ -7,6 +8,79 @@ namespace CommandPattern
     {
         void Execute();
         bool CanExecute();
+    }
+
+
+    public class CompositeCommand : ICommand
+    {
+        Queue<ICommand> commands = new Queue<ICommand>();
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public void Execute()
+        {
+            while (commands.Count > 0)
+            {
+                ICommand command = commands.Dequeue();
+
+                if (command.CanExecute())
+                    command.Execute();
+            }
+        }
+
+        public void Register(ICommand command)
+        {
+            commands.Enqueue(command);
+        }
+    }
+
+
+    public class LoginAndPrintCommand : ICommand
+    {
+        private readonly Context context;
+
+        public LoginAndPrintCommand(Context context)
+        {
+            this.context = context;
+        }
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public void Execute()
+        {
+            ICommand loginCommand = new LoginCommand(context);
+            ICommand printCommand = new PrintCommand(context.Message);
+
+            loginCommand.Execute();
+            printCommand.Execute();
+        }
+    }
+
+    public class LoginCommand : ICommand
+    {
+        private readonly Context context;
+
+        public LoginCommand(Context context)
+        {
+            this.context = context;
+        }
+
+        public bool CanExecute()
+        {
+            return true;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Login");
+            context.User = "john";
+        }
     }
 
 
@@ -35,11 +109,13 @@ namespace CommandPattern
     public class PrintCommand : ICommand
     {
         private readonly Message message;
+        private readonly string user;
         private readonly byte copies;
 
         public PrintCommand(Message message, byte copies = 1)
         {
             this.message = message;
+            this.user = user;
             this.copies = copies;
         }
 
@@ -52,7 +128,7 @@ namespace CommandPattern
         {
             for (int i = 0; i < copies; i++)
             {
-                Console.WriteLine($"Print message from <{message.From}> to <{message.To}> {message.Content}");
+                Console.WriteLine($"Print message from <{message.From}> to <{message.To}> {message.Content} by {user}");
             }
         }
     }
@@ -69,7 +145,14 @@ namespace CommandPattern
         public string From { get; set; }
         public string To { get; set; }
         public string Content { get; set; }
-     
+       
+
+    }
+
+    public class Context
+    {
+        public Message Message { get; set; }
+        public string User { get; set; }
     }
 
 }
--- src/03_BehavioralsPatterns/CommandPattern/Program.cs
+++ src/03_BehavioralsPatterns/CommandPattern/Program.cs
@@ -10,20 +10,24 @@ namespace CommandPattern
             Console.WriteLine("Hello Command Pattern!");
 
             Message message = new Message("555000123", "555888000", "Hello World!");
+            Context context = new Context { Message = message };
 
-            ICommand printCommand  = new PrintCommand(message);
-            ICommand sendCommand = new SendCommand(message); 
+            ICommand loginCommand = new LoginCommand(context);
+            ICommand printCommand = new PrintCommand(context.Message);
+            ICommand sendCommand = new SendCommand(message);
+            ICommand loginAndPrintCommand = new LoginAndPrintCommand(context);
+
+            //CompositeCommand loginAndPrintCommand = new CompositeCommand();
+            //loginAndPrintCommand.Register(loginCommand);
+            //loginAndPrintCommand.Register(printCommand);
 
             Queue<ICommand> commands = new Queue<ICommand>();
-            commands.Enqueue(printCommand);
-            commands.Enqueue(printCommand);
-            commands.Enqueue(printCommand);
-            commands.Enqueue(sendCommand);
+            commands.Enqueue(loginAndPrintCommand);
 
-            while(commands.Count > 0)
+            while (commands.Count > 0)
             {
                 ICommand command = commands.Dequeue();
-                
+
                 if (command.CanExecute())
                     command.Execute();
             }
